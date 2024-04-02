b20dec662ac51983ee18285e391022366c142ce6
Utsav <ukumar380@gmail.com>
2024-04-02 16:09:38 +0530

    export added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const App = () => {
   const [apkPath, setApkPath] = useState('');
@@ -9,58 +14,10 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const [open, setOpen] = React.useState(false);
 
-    try {
-      const response = await axios.get('http://localhost:3001/install-apk', {
-        params: { apkPath: apkPath },
-      });
-      alert(response.data)
-      // alert(`Installed APK: ${response.data} on device: ${response.data}`);
 
-      // Clear the input field after successful installation
-      setApkPath('');
-
-      // Update the device ID after successful installation
-      setDeviceId(response.data.deviceId);
-
-    } catch (error) {
-      if (error.response) {
-        console.error('Error:', error.response.status, error.response.statusText);
-        if (error.response.data && error.response.data.error) {
-          alert(`Error occurred while installing APK: ${error.response.data.error}`);
-        } else {
-          alert(`Error occurred while installing APK: ${error.response.status} ${error.response.statusText}`);
-        }
-      } else if (error.request) {
-        console.error('Error:', 'No response received from the server.');
-        alert('Error occurred while installing APK: No response received from the server.');
-      } else {
-        console.error('Error:', error.message);
-        alert(`Error occurred while installing APK: ${error.message}`);
-      }
-    
-      setApkPath('');
-    }
-  };
-
-  const handleFetchLogs = async () => {
-    setLoading(true);
-    try {
-        const response = await axios.get('http://localhost:3001/device-logs');
-        console.log(response);
-        // setLogs(response.data.logs);
-        setError('');
-    } catch (error) {
-      setLogs(error.response.data.logs);
-        // setLogs(error.response.data.logs);
-        setError('Error fetching logs');
-    }
-    setLoading(false);
-};
-
-// Fetch device ID when component mounts
+  const notify = (req) => toast(req);
 
   useEffect(() => {
     const fetchDeviceID = async () => {
@@ -69,7 +26,8 @@ const App = () => {
               .then(response => {
                 if (response.data && response.data.deviceID) {
                   setDeviceId(response.data.deviceID);
-                  alert(`Device : ${response.data.deviceID} connected`)
+                  notify(`Device : ${response.data.deviceID} connected`);
+                  // alert(`Device : ${response.data.deviceID} connected`)
                 }
             })
             .catch(err => {
@@ -85,10 +43,106 @@ const App = () => {
     fetchDeviceID();
   }, []);
 
+  //INSTALL APP
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setOpen(true);
+
+    try {
+      // notify();
+      const response = await axios.get('http://localhost:3001/install-apk', {
+        params: { apkPath: apkPath },
+      });
+      // alert(response.data)
+      notify(response.data);
+      // alert(`Installed APK: ${response.data} on device: ${response.data}`);
+
+      // Clear the input field after successful installation
+      setApkPath('');
+
+      // Update the device ID after successful installation
+      // setDeviceId(response.data.deviceId);
+
+    } catch (error) {
+      if (error.response) {
+        console.error('Error:', error.response.status, error.response.statusText);
+        if (error.response.data && error.response.data.error) {
+          // alert(`Error occurred while installing APK: ${error.response.data.error}`);
+          notify(`Error occurred while installing APK: ${error.response.data.error}`);
+          setDeviceId('')
+        } else {
+          // alert(`Error occurred while installing APK: ${error.response.status} ${error.response.statusText}`);
+          notify(`Error occurred while installing APK: ${error.response.status} ${error.response.statusText}`)
+        }
+      } else if (error.request) {
+        console.error('Error:', 'No response received from the server.');
+        notify('Error occurred while installing APK: No response received from the server.');
+        // alert('Error occurred while installing APK: No response received from the server.');
+      } else {
+        console.error('Error:', error.message);
+        alert(`Error occurred while installing APK: ${error.message}`);
+      }
+
+    
+      setApkPath('');
+    }
+    setOpen(false);
+  };
+
+
+  const handleFetchLogs = async () => {
+    setOpen(true);
+    setLoading(true);
+    try {
+        const response = await axios.get('http://localhost:3001/device-logs');
+        console.log(response);
+        // setLogs(response.data.logs);
+        setError('');
+        setOpen(false);
+    } catch (error) {
+      console.log('Here   ',error);
+      if(error.message==='Network Error'){
+        notify('Device Connection Failed');
+      }
+      else{
+      if(error.response.data){
+        setLogs(error.response.data.logs)
+        notify('Logs taken successfully!')
+      }
+      // setLogs(error.response.data.logs);
+      //   // setLogs(error.response.data.logs);
+      //   setError('Error fetching logs');
+    }
+  }
+  setOpen(false);
+  setLoading(false);
+  };
+
+  function eraseText() {
+    document.getElementById("logTextArea").value = "";
+}
+
+
+
+const writeToFile = async () => {
+  try {
+    const response = await axios.post('http://localhost:3001/export-logs', { logs });
+    notify(response.data.message);
+  } catch (error) {
+    console.error(error);
+    notify('Error exporting logs to file');
+  }
+};
 
   return (
-    <div class="container">
-      <span>  </span>
+<div class='bg-light'>
+      <nav class="navbar navbar-light bg-dark">
+  <div class="container">
+    <span class="navbar-brand mb-0 h1 bg-dark">ADBWeb</span>
+  </div>
+</nav>
+
+<div class="container-sm">
   <div class="jumbotron">
     <form onSubmit={handleSubmit}>
       <div class="row mb-3">
@@ -96,15 +150,15 @@ const App = () => {
         <div class="col-sm-10">
           {deviceId ? (
             <input
-              type="text"
-              class="form-control"
-              id="deviceId"
-              value={deviceId}
-              readonly
+            type="text"
+            class="form-control"
+            id="deviceId"
+            value={deviceId}
+            readonly
             />
-          ) : (
-            <span>No device connected</span>
-          )}
+            ) : (
+              <span>No device connected</span>
+              )}
         </div>
       </div>
       <div class="mb-3">
@@ -115,23 +169,39 @@ const App = () => {
           id="apkPath"
           value={apkPath}
           onChange={(e) => setApkPath(e.target.value)}
-        />
+          />
       </div>
       <button class="btn btn-primary" type="submit">Install APK</button>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+        >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      {open && <Backdrop/>}
     </form>
     <div class="mb-3">
       <label for="logTextArea" class="form-label">Logs:</label>
       <textarea class="form-control" 
       id="logTextArea" 
       rows="10" 
+      wrap="off" 
+      cols="30"
       value={logs.join('\n')}
       readOnly
       ></textarea>
     </div>
-      <button class="btn btn-primary" onClick={handleFetchLogs}>Fetch Logs</button>
+      <div>
+      <button class="btn btn-primary left" onClick={handleFetchLogs}>Fetch Logs</button>
+      &nbsp;
+      <button class='btn btn-secondary' onClick={eraseText}>Clear</button>
+      &nbsp;
+      <button className='btn btn-success' onClick={writeToFile}>Export Logs</button>
+      </div>
   </div>
+  <ToastContainer />
 </div>
-
+      </div>
 
   );
 };
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const logcat = require('adbkit-logcat')
 const { spawn } = require('child_process');
 const client = adb.createClient();
 var cors = require('cors');
+const fs = require('fs');
 
 const app = express();
 
@@ -57,57 +58,110 @@ app.get('/device-id', async(req, res) => {
 });
 
 //LOGS PART
-app.get('/device-logs', (req, res) => {
-  const proc = spawn('adb', ['logcat', '-B', 'all' , '*:F', '*:E']);
+app.get('/device-logs', async (req, res) => {
+  try {
+    const proc = spawn('adb', ['logcat', '-B', 'all', '*:F', '*:E']);
+    const reader = logcat.readStream(proc.stdout);
+    let crashDetected = false;
+    let logs = [];
 
-  // Connect logcat to the stream
-  const reader = logcat.readStream(proc.stdout);
+    reader.on('entry', entry => {
+      console.log(entry);
+      // if (entry.priority === 'F' || entry.priority === 'E' || entry.message.includes('com.jio.photos')) {
+      if (entry.priority === 7 || entry.priority === 6) {
+        console.log('Crash detected');
+        console.log(entry.message);
+        logs.push(entry.message);
+        crashDetected = true;
+        proc.kill();
+      }
+    });
 
-  let crashDetected = false; // Flag to track if a crash is detected
-  let logs=[]
+    let responseSent = false;
 
-  // Event handler for logcat entries
-  reader.on('entry', entry => {
-    console.log(entry);
-    
-    // Check if the log entry indicates a crash
-    if (entry.priority === 'F' || entry.priority === 'E' || entry.message.includes('com.jio.photos')) {
-      console.log('Crash detected');
-      console.log(entry.message);
-
-      logs.push(entry.message);
-      crashDetected = true;
-      proc.kill(); // Kill the logcat process
-    }
-  });
-
-  let responseSent = false; // Flag to track whether response has been sent
-
-  proc.on('exit', (code, signal) => {
-      console.log(`Logcat process exited with code ${code} and signal ${signal}`);
-  
-      // If the process exited but no crash was detected and response hasn't been sent yet, handle it here
-      if (!crashDetected && !responseSent) {
-          // Handle the case where logcat exited without detecting a crash
+    await new Promise((resolve, reject) => {
+      proc.on('exit', (code, signal) => {
+        console.log(`Logcat process exited with code ${code} and signal ${signal}`);
+        if (!crashDetected && !responseSent) {
           console.log('No crash detected');
-          responseSent = true; // Set flag to true to indicate response has been sent
+          responseSent = true;
           res.status(200).json({ logs: logs, message: 'No crash detected' });
-      }
-  });
-  
-  // Send response if a crash is detected and response hasn't been sent yet
-  reader.on('end', () => {
-      if (crashDetected && !responseSent) {
-          responseSent = true; // Set flag to true to indicate response has been sent
+          resolve();
+        }
+      });
+
+      reader.on('end', () => {
+        if (crashDetected && !responseSent) {
+          responseSent = true;
           res.status(500).json({ logs: logs, message: 'Crash detected' });
-      }
-  });
+          resolve();
+        }
+      });
+    });
+  } catch (error) {
+    console.error('Error occurred:', error);
+    res.status(500).json({ error: 'An error occurred while fetching device logs' });
+  }
 });
+// app.get('/device-logs', (req, res) => {
+//   const proc = spawn('adb', ['logcat', '-B', 'all' , '*:F', '*:E']);
+
+//   // Connect logcat to the stream
+//   const reader = logcat.readStream(proc.stdout);
+
+//   let crashDetected = false; // Flag to track if a crash is detected
+//   let logs=[]
+
+//   // Event handler for logcat entries
+//   reader.on('entry', entry => {
+//     console.log(entry);
+    
+//     // Check if the log entry indicates a crash
+//     if (entry.priority === 'F' || entry.priority === 'E' || entry.message.includes('com.jio.photos')) {
+//       console.log('Crash detected');
+//       console.log(entry.message);
+
+//       logs.push(entry.message);
+//       crashDetected = true;
+//       proc.kill(); // Kill the logcat process
+//     }
+//   });
+
+//   let responseSent = false; // Flag to track whether response has been sent
+
+//   proc.on('exit', (code, signal) => {
+//       console.log(`Logcat process exited with code ${code} and signal ${signal}`);
+  
+//       // If the process exited but no crash was detected and response hasn't been sent yet, handle it here
+//       if (!crashDetected && !responseSent) {
+//           // Handle the case where logcat exited without detecting a crash
+//           console.log('No crash detected');
+//           responseSent = true; // Set flag to true to indicate response has been sent
+//           res.status(200).json({ logs: logs, message: 'No crash detected' });
+//       }
+//   });
+  
+//   // Send response if a crash is detected and response hasn't been sent yet
+//   reader.on('end', () => {
+//       if (crashDetected && !responseSent) {
+//           responseSent = true; // Set flag to true to indicate response has been sent
+//           res.status(500).json({ logs: logs, message: 'Crash detected' });
+//       }
+//   });
+// });
 
 
 //INSTALL PART
 app.get('/install-apk', async (req, res) => {
   // console.log(req.query.apk)
+  const devices = await client.listDevices();
+
+    if (devices.length === 0) {
+      res.status(404).json({error:'No devices connected'});
+      
+      return;
+    }
+  
   const apk = req.query.apkPath;
   if (!apk) {
     res.status(400).json({error:'Missing APK path'});
@@ -116,13 +170,6 @@ app.get('/install-apk', async (req, res) => {
 
   try {
     const devices = await client.listDevices();
-
-    if (devices.length === 0) {
-      res.status(404).json({error:'No devices connected'});
-      
-      return;
-    }
-
     const device = devices[0];
 
     await client.install(device.id, apk);
@@ -130,10 +177,37 @@ app.get('/install-apk', async (req, res) => {
     console.log(`Installed ${apk} on device ${device.id}`);
     res.send(`Installed ${apk} on device ${device.id}`);
   } catch (err) {
-    console.error('Something went wrong:', err.stack);
-    res.status(500).json({error:'Something went wrong'});
+    console.error('Lost Connection with Device', err.stack);
+    res.status(500).json({error:'Lost Connection with Device'});
   }
 });
+
+app.post('/export-logs', (req, res) => {
+  try {
+    const logs = req.body.logs;
+    if (!logs || !Array.isArray(logs)) {
+      return res.status(400).json({ error: 'Logs data is missing or invalid' });
+    }
+
+    const fileName = 'device_logs.txt'; // Name of the file to be exported
+    const filePath = './logs' + '/' + fileName; // Path where the file will be saved
+
+    // Write logs data to the file
+    const data = logs.join('\n'); // Join log messages with newline character
+    fs.writeFile(filePath, data, err => {
+      if (err) {
+        console.error('Error writing logs to file:', err);
+        return res.status(500).json({ error: 'An error occurred while exporting logs' });
+      }
+      console.log('Logs have been exported to file:', fileName);
+      res.status(200).json({ message: 'Logs exported successfully', file: fileName });
+    });
+  } catch (error) {
+    console.error('Error occurred:', error);
+    res.status(500).json({ error: 'An error occurred while exporting logs' });
+  }
+});
+
 
 app.listen(3001, () => {
   console.log('App listening on port 3001!');
